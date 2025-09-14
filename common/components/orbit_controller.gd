df0bf7a25f91c86d45adcdef7eed4cb3f4f8203f
MidnightBlue <113545024+R1fty0@users.noreply.github.com>
2025-09-14 16:07:39 -0700

    feat: made objects orbiting the planet face in their direction of motion

--- common/components/orbit_controller.gd
+++ common/components/orbit_controller.gd
@@ -7,7 +7,7 @@ class_name OrbitController
 ## How high above the planet is the object orbiting. 
 @export var orbit_radius: float = 1.0
 ## How fast does the object move while orbiting. 
-@export var orbital_velocity: float = 2.0
+@export var orbit_velocity: float = 2.0
 
 # TODO: make getting this cleaner 
 @export var planet_center: Node3D
@@ -16,13 +16,18 @@ var orbit_angle: float = 0.0
 
 func _physics_process(delta: float) -> void:
 	# Update orbit angle
-	orbit_angle += orbital_velocity * delta
-	
+	orbit_angle += orbit_velocity * delta
 	var rotation = Basis().rotated(orbit_axis.normalized(), orbit_angle)
 	# TODO: fix this so that planet position doesn't have to be at (0,0,0)
 	var offset = rotation * Vector3(0,0, orbit_radius)
-	var new_pos = offset + planet_center.position
-	
-	# Set new position
+	var new_pos = offset + planet_center.global_position
+	# Get the object's last position. 
+	var old_pos = get_parent().global_position
+	# Figure out the rotation needed for the object to be pointing at its new position. 
+	var direction = (new_pos - old_pos).normalized()
+	# Apply the rotation.
+	if direction.length() > 0:
+		get_parent().look_at(new_pos + direction, orbit_axis)
+	# Apply the new position. 
 	get_parent().global_position = new_pos
 	
